dcbcd1ab38eeae9f36f746c9631fb7c1dfed0187
artkoder <md.nikiforov@gmail.com>
2025-10-12 00:08:32 +0200

    fix(viewer): add compose runtime dependencies

--- feature/viewer/build.gradle.kts
+++ feature/viewer/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.runtime.saveable)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.navigation)
