a999c8e96319b5dcb1afbdb99b481e5526be99bb
artkoder <md.nikiforov@gmail.com>
2025-10-15 16:32:07 +0200

    Use WorkManager progress API available in runtime

--- core/network/src/main/java/com/kotopogoda/uploader/core/network/work/DeleteRequestHelperActivity.kt
+++ core/network/src/main/java/com/kotopogoda/uploader/core/network/work/DeleteRequestHelperActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.work.Data
 import androidx.work.WorkManager
-import androidx.work.setProgress
 import java.util.UUID
 
 internal class DeleteRequestHelperActivity : Activity() {
@@ -92,7 +91,7 @@ internal class DeleteRequestHelperActivity : Activity() {
             builder.putByteArray(DeleteRequestContract.KEY_PENDING_DELETE_INTENT, ByteArray(0))
             builder.putLong(DeleteRequestContract.KEY_PENDING_DELETE_LAST_LAUNCH, 0L)
         }
-        WorkManager.getInstance(applicationContext).setProgress(id, builder.build())
+        WorkManager.getInstance(applicationContext).setProgressWorkDataById(id, builder.build())
     }
 
     companion object {
