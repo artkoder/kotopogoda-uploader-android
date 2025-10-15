e545f50e1ee94c7bccbef9d9f935493d5bcc0caf
artkoder <md.nikiforov@gmail.com>
2025-10-15 13:39:42 +0200

    Remove invalid setProgress import

--- core/network/src/main/java/com/kotopogoda/uploader/core/network/work/DeleteRequestHelperActivity.kt
+++ core/network/src/main/java/com/kotopogoda/uploader/core/network/work/DeleteRequestHelperActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.work.Data
 import androidx.work.WorkManager
-import androidx.work.setProgress
 import java.util.UUID
 
 internal class DeleteRequestHelperActivity : Activity() {
